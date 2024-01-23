153f313f0116ccd5b3052031aaaf81e1e8d2bc47
ThanhThy <thy.nnt.63cntt@ntu.edu.vn>
2024-01-23 23:55:31 +0700

    Ví dụ đầu tiên
    DEMO

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 63131425-AndroidProgramming
 Lưu các bài thực hành 
+- 1 2 3 check 
---
+++ Tuan2Android/src/Aroid2/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Aroid2;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("LTTDD");
+	}
+
+}
